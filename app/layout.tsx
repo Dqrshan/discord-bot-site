9a0c6f5626d69e92838f12c6055aee2c591887cc
Lorenz <darshan310268@gmail.com>
2023-06-13 18:27:09 +0530

    core: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,33 @@ import { Outfit } from "next/font/google";
 
 const inter = Outfit({ subsets: ["latin"], weight: ["500"] });
 
-export const metadata: Metadata = {
+const data = {
     title: "Discord Bot",
-    description: "A simple Discord bot website",
-    authors: [{ name: "Lorenz", url: "https://darshan.studio/" }]
+    description: "A simple discord bot site",
+    image: "https://static.vecteezy.com/system/resources/previews/006/892/625/non_2x/discord-logo-icon-editorial-free-vector.jpg",
+    theme: "#7dd3fc"
+};
+
+export const metadata: Metadata = {
+    title: data.title,
+    description: data.description,
+    authors: [{ name: "Lorenz", url: "https://darshan.studio/" }],
+    themeColor: data.theme,
+    openGraph: {
+        title: data.title,
+        description: data.description,
+        images: data.image,
+        type: "website",
+        siteName: data.title,
+        url: "https://dash-bot.vercel.app/"
+    },
+    twitter: {
+        title: data.title,
+        description: data.description,
+        images: data.image,
+        card: "summary",
+        creator: "Lorenz"
+    }
 };
 
 export default function RootLayout({
